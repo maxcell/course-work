9e9e3fca0d73f6f6024c36dead2087e395c2fe47
Prince Wilson <maxcell.wilson@gmail.com>
2021-11-07 19:12:11 -0500

    Learn about transforming objects

--- main.js
+++ main.js
@@ -7,28 +7,45 @@ const sizes = {
   height: 800
 }
 
+const camera = new THREE.PerspectiveCamera(
+  45, // FOV
+  sizes.width / sizes.height,
+  0.1,
+  1000
+)
+
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const mesh = new THREE.MeshBasicMaterial({
   color: 'red'
 })
 
 const cube = new THREE.Mesh(geometry, mesh)
+camera.position.z = 10
+cube.position.set(1, -0.6, 3)
+cube.scale.set(2, 0.5, 0.5)
 
+cube.rotation.y = 1
 
+// Look into Gimbal Lock
+// To avoid this is, you need to use reorder
+// Apply the reorder prior to any rotation
 scene.add(cube)
-const camera = new THREE.PerspectiveCamera(
-  75, // FOV
-  sizes.width / sizes.height,
-  0.1,
-  1000
-)
 
-camera.position.z = 5
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
+// Camera's position is actually a vector
+// Distance between scene center and object is length
+// Distance between object and camera is distanceTo
+// camera.position.x = 0.7
+// camera.position.y = -0.5
+// camera.position.z = 5
+
+// Vectors can be manually changed around as well
 scene.add(camera)
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById('canvas')
 })
 
-renderer.setSize( sizes.width, sizes.height)
+renderer.setSize(sizes.width, sizes.height)
 
 renderer.render(scene, camera) 
