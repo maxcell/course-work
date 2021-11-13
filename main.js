f32b5414e5573876a87f605880ee474b87398b05
Prince Wilson <maxcell.wilson@gmail.com>
2021-11-13 17:02:34 -0500

    Adds Orbit controls to learn about mouse based cameras

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 import * as THREE from 'three';
-
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 const scene = new THREE.Scene();
+
+const domElement =  document.getElementById('canvas')
+
+
+const cursor = { x: 0, y: 0 }
 
 const sizes = {
   width: 800,
   height: 600
 }
 
-// const camera = new THREE.PerspectiveCamera(
-//   75, // FOV
-//   sizes.width / sizes.height,
-//   0.1,
-//   100
-// )
-const aspectRatio = sizes.width / sizes.height
-const camera = new THREE.OrthographicCamera(
-  -1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 1000
-)
+window.addEventListener('mousemove', (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5 // midpoint
+  cursor.y = event.clientY / sizes.height - 0.5 // midpoint
+})
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const mesh = new THREE.MeshBasicMaterial({
@@ -24,23 +23,37 @@ const mesh = new THREE.MeshBasicMaterial({
 })
 
 const cube = new THREE.Mesh(geometry, mesh)
-camera.position.z = 10
+
+    // const aspectRatio = sizes.width / sizes.height
+    //   const camera = new THREE.OrthographicCamera(
+    //     -1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 1000
+    //   )
+const camera = new THREE.PerspectiveCamera(
+  75, // FOV
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.z = 3
+
+const orbitControls = new OrbitControls(camera, domElement)
+orbitControls.enableDamping = true
 
 scene.add(cube)
 
 // Vectors can be manually changed around as well
 scene.add(camera)
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.getElementById('canvas')
+  canvas: domElement
 })
 
 renderer.setSize(sizes.width, sizes.height)
 
 let clock = new THREE.Clock()
 function loop() {
-  camera.position.x = Math.cos(clock.getElapsedTime()) * Math.PI
-  camera.position.y = Math.sin(clock.getElapsedTime()) * Math.PI
-  camera.lookAt(cube.position)
+orbitControls.update()
+
+  // cube.rotation.y = clock.getElapsedTime() * Math.PI * 0.5
   renderer.render(scene, camera)
   window.requestAnimationFrame(loop)
 }
